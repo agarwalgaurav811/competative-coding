17aedd732dd4e49b28ee59f773f603f48376f951
gaurav agarwal <agarwalgaurav811@gmail.com>
2018-03-27 23:05:21 +0530

    Update container_extensive.cpp

--- container_extensive.cpp
+++ container_extensive.cpp
@@ -11,21 +11,26 @@ vector<vector<int> > Solution::levelOrder(TreeNode* A) {
     
     queue < pair<__typeof(A),int> > q;
     q.push(make_pair(A,0));
-    map<int,vector<int> > M;
+    map<long long,vector<int> > M;
     while(!q.empty())
     {
         auto(v) = q.front().first;
         int cur_level = q.front().second;
         q.pop();
+        if(v!=NULL){
         if(M.find(cur_level)!=M.end())
             M[cur_level].push_back(v->val);
-        else
-            M[cur_level]= vector<int>();
+        else{
+            M[cur_level]= vector<int>(1,v->val);
+           // M[cur_level].clear();
+        }
         q.push(make_pair(v->left,cur_level+1));
         q.push(make_pair(v->right,cur_level+1));
+        }
     }
     vector<vector<int> > ans(M.size());
-    for(int i=0;i<M.size();i++){
+    for(long long i=0;i<M.size();i++){
+      //  cout<<M[i][0]<<endl;
         ans[i]=vector<int>(M[i].begin(),M[i].end());
     }
     return ans;
