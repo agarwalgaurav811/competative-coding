930417f0fe713f372a0c90d0b9bc6179934a5877
gaurav agarwal <agarwalgaurav811@gmail.com>
2018-03-27 20:55:26 +0530

    Create container_extensive.cpp

---
+++ container_extensive.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+vector<vector<int> > Solution::levelOrder(TreeNode* A) {
+    
+    queue < pair<__typeof(A),int> > q;
+    q.push(make_pair(A,0));
+    map<int,vector<int> > M;
+    while(!q.empty())
+    {
+        auto(v) = q.front().first;
+        int cur_level = q.front().second;
+        q.pop();
+        if(M.find(cur_level)!=M.end())
+            M[cur_level].push_back(v->val);
+        else
+            M[cur_level]= vector<int>();
+        q.push(make_pair(v->left,cur_level+1));
+        q.push(make_pair(v->right,cur_level+1));
+    }
+    vector<vector<int> > ans(M.size());
+    for(int i=0;i<M.size();i++){
+        ans[i]=vector<int>(M[i].begin(),M[i].end());
+    }
+    return ans;
+}
